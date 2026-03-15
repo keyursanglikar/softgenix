5a37c2a62c880c849c629c22e06efd3a9b13f473
keyursanglikar <sanglikarkeyur@gmail.com>
2026-03-15 06:55:37 +0530

    Update Vite config and package.json for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Important: ensures assets work in static deploy
+  build: {
+    outDir: 'dist', // default output folder for Vercel
+  },
 })
